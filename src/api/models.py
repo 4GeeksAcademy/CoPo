5810c0fabf59d1f51a80e56145bfda57448a8ed7
Brandon-Ray Brown <77804389+BeCoded126@users.noreply.github.com>
2025-04-26 01:33:05 +0000

    updated show endpoint

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+
+# will need a relationship and table name for the user object in the favorites model
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -20,3 +22,30 @@ class User(db.Model):
             "age": self.age
             # do not serialize the password, its a security breach
         }
+
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # user:
+    show = relationship("Show",backref="favorites")
+
+  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "show": [item.showTitle for item in self.show] 
+         }
+    
+
+class Show(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    showTitle: Mapped[str] = mapped_column(String(50), unique=False, nullable=True)
+    favoriteId: Mapped[int] = mapped_column(ForeignKey("favorites.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "showTitle": self.showTitle,
+            "favoriteId": self.favoriteId,
+            }
