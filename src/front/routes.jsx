e44b9c4872c7f46e1dee0564092c1f9f23f8ce58
Sblas <shaelynb0405@gmail.com>
2025-05-17 00:48:22 +0000

    changed home.jsx and fixed routes

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -28,13 +28,13 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Signup />} />
+        <Route path= "/signup" element={<Signup />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
         <Route path="/resetpassword" element={<PasswordRecovery />} />
         <Route path="/profile" element={ <Profile />} />
         <Route path="/login" element={ <LoginPage />} />
-        <Route path="/home" element={ <HomePage />} />
+        <Route path="/" element={ <HomePage />} />
         {/* <Route path="/chat" element={ <Chatdemo />} /> */}
       </Route>
     )
