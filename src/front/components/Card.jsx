dd7515cadef6f0443e7153b3b277e103241a9fe3
Sblas <shaelynb0405@gmail.com>
2025-05-21 01:32:42 +0000

    trying to fix store.js and link to favorites list

--- src/front/components/Card.jsx
+++ src/front/components/Card.jsx
@@ -1,29 +1,34 @@
 // export is to be able to use in other components 
-export const Card = ({title,id,end}) => {
-                    //the 3 variables used for making the tags dynamic 
-    return (
-        <div>
-            <div className="w-100 card" >
-                <img src={`https://cdn.watchmode.com/posters/0${id}_poster_w185.jpg`} class="card-img-top" alt="..."/>
-                {/*can also add extra variable for the image source  */}
-                        <div className="card-body">
-                        <p className="card-title">{title}</p>
-                        <p className="card-text">{}</p>
-                        <a href="#" class="btn btn-primary">{end}</a>
-                       
-                       {/* dispatch: is like calling a helper and notifying of an update/add on 
+export const Card = ({ title, id, end }) => {
+  //the 3 variables used for making the tags dynamic 
+  return (
+    <div>
+      <div className="w-100 card" >
+        <img src={`https://cdn.watchmode.com/posters/0${id}_poster_w185.jpg`} class="card-img-top" alt="..." />
+        {/*can also add extra variable for the image source  */}
+        <div className="card-body">
+          <p className="card-title">{title}</p>
+          <p className="card-text">{ }</p>
+
+          {/* dispatch: is like calling a helper and notifying of an update/add on 
                        type: is saying put whatever is in here into my favorites box 
                        payload: is saying to add this SHOW-TITLE to my favorites*/}
-                        <button
-                      onClick={() =>
-                        dispatch({ type: "set_Fav", payload: show.title }) 
-                      }
-                    >
-                      {" "}
-                      <i className="fa-solid fa-heart"> </i>{" "}
-                    </button>
-                    </div>
-            </div>
+
+          <div className="dropdown d-flex mx-auto ">
+            <a href="#" class="btn btn-primary">{end}Watch Me</a>
+            <button
+              className="btn btn-success"
+              type="button"
+              onClick={() =>
+                dispatch({ type: "set_Fav", payload: show.title })
+              }
+            >
+              {" "}
+              <i className="fa-solid fa-heart"> </i>{" "}
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/front/store.js
+++ src/front/store.js
@@ -1,69 +1,63 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ],
-    favorites: null,
-  }
-}
-
-export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'set_hello':
-      return {
-        ...store,
-        message: action.payload
-      };
-      
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
-    default:
-      throw Error('Unknown action.');
-    case 'set_favs': 
-      return {
-        ...store,
-        favorites: action.payload
-      }
-  }    
-}
-
-// state = your notebook (it holds your favorites list)
-// action = a message you send to the librarian telling them what to do
-
-
-// function setFav(store, action) {
-//   switch (action.type) {
-//     case "my-likes":
-//       return {
-//         ...state,
-//         favorites: [...store.favorites, action.payload],
-//       };
-//     // other cases...
-//     default:
-//       return state;
+// export const initialStore=()=>{
+//   return{
+//     message: null,
+//     todos: [
+//       {
+//         id: 1,
+//         title: "Make the bed",
+//         background: null,
+//       },
+//       {
+//         id: 2,
+//         title: "Do my homework",
+//         background: null,
+//       }
+//     ],
+//     favorites: null,
 //   }
 // }
 
+// export default function storeReducer(store, action = {}) {
+//   switch(action.type){
+//     case 'set_hello':
+//       return {
+//         ...store,
+//         message: action.payload
+//       };
+      
+//     case 'add_task':
 
-//  if(action.type == "my-likes"){
-//   if(store?.favorites.includes(action.payload)){return store}
+//       const { id,  color } = action.payload
+
+//       return {
+//         ...store,
+//         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+//       };
+//     default:
+//       throw Error('Unknown action.');
+//     case 'set_favs': 
+//       return {
+//         ...store,
+//         favorites: action.payload
+//       }
+//   }    
+// }
+
+// state = your notebook (it holds your favorites list)
+// action = a message you send to the librarian telling them what to do
+ 
+ if(action.type == "set_Fav"){
+  if(store?.fav.includes(action.set_Fav)){return store}
+  return{
+    ...store,
+    fav: [...store.fav,action.set_Fav]
+  }
+ }
+ 
+//  if(action.type == "dislike"){
 //   return{
 //     ...store,
-//     favorites: [...store.favorites,action.payload]
+//     favorites: store.favorites.filter((item)=> item != action.payload)
 //   }
+//  }
+//        ^--this is the remove feature 
